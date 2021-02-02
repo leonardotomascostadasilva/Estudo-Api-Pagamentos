8e79dc270fb34fe33867d43f9e4f91b8b4e1f801
Leonardo Tomas Costa da Silva <76014605+leonardotomascostadasilva@users.noreply.github.com>
2021-02-02 20:00:13 -0300

    teste cpf e cnpj

---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsValid()
+        {
+            var doc = new Document("95016331000126", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsValid()
+        {
+            var doc = new Document("85497452000", EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
