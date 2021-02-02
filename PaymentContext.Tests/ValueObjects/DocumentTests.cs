462e9d50f166b3dc12bfd808eb18a4a52696e00c
Leonardo Tomas Costa da Silva <76014605+leonardotomascostadasilva@users.noreply.github.com>
2021-02-02 20:14:03 -0300

    Renomear funções

--- PaymentContext.Tests/ValueObjects/DocumentTests.cs
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -1,36 +1,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PaymentContext.Domain.Entities;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
 
-namespace PaymentContext.Tests
+namespace PaymentContext.Tests.ValueObjects
 {
     [TestClass]
     public class DocumentTests
     {
         [TestMethod]
-        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        public void ShouldReturnErrorWhenCnpjIsInvalid()
         {
             var doc = new Document("123", EDocumentType.CNPJ);
             Assert.IsTrue(doc.Invalid);
         }
 
         [TestMethod]
-        public void ShouldReturnErrorWhenCNPJIsValid()
+        public void ShouldReturnErrorWhenCnpjIsValid()
         {
             var doc = new Document("95016331000126", EDocumentType.CNPJ);
             Assert.IsTrue(doc.Valid);
         }
 
         [TestMethod]
-        public void ShouldReturnErrorWhenCPFIsInvalid()
+        public void ShouldReturnErrorWhenCpfIsInvalid()
         {
             var doc = new Document("123", EDocumentType.CPF);
             Assert.IsTrue(doc.Invalid);
         }
 
         [TestMethod]
-        public void ShouldReturnErrorWhenCPFIsValid()
+        public void ShouldReturnErrorWhenCpfIsValid()
         {
             var doc = new Document("85497452000", EDocumentType.CPF);
             Assert.IsTrue(doc.Valid);
